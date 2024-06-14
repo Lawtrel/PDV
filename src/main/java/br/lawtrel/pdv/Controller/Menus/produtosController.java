ff8d30a0b97746d44043e03227d268faf2a8a17d
CaiqueSil <165158889+CaiqueSil@users.noreply.github.com>
2024-06-14 15:57:22 -0300

    1.1.0

--- src/main/java/br/lawtrel/pdv/Controller/Menus/produtosController.java
+++ src/main/java/br/lawtrel/pdv/Controller/Menus/produtosController.java
@@ -1,12 +1,17 @@
 package br.lawtrel.pdv.Controller.Menus;
 
 import br.lawtrel.pdv.Model.Produto;
+import br.lawtrel.pdv.Model.connectDB;
+import br.lawtrel.pdv.Model.dao.ProdutoDao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class produtosController {
     @FXML
@@ -25,6 +30,22 @@ public class produtosController {
     @FXML
     private TableColumn<Produto, Integer>  quantidadecolumn;
 
+    private final ObservableList<Produto> produtosList;
+
+    private final Connection connection;
+    private final ProdutoDao produtoDao;
+    public produtosController() throws SQLException {
+        connection = connectDB.getConnection();
+        produtoDao = new ProdutoDao(connection);
+        produtosList = FXCollections.observableArrayList();
+
+    }
+
+    public produtosController(ObservableList<Produto> produtosList, Connection connection, ProdutoDao produtoDao) {
+        this.produtosList = produtosList;
+        this.connection = connection;
+        this.produtoDao = produtoDao;
+    }
 
     @FXML
     protected void swapprodutos(ActionEvent e){
