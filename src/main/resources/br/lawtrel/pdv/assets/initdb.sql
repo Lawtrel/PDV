9c9c9fbc21a7eb61fef8c7eb8edbf15b95c38482
Lawtrel <kazemarus@outlook.com>
2024-06-17 12:06:08 -0300

    update  query

--- src/main/resources/br/lawtrel/pdv/assets/initdb.sql
+++ src/main/resources/br/lawtrel/pdv/assets/initdb.sql
@@ -1,10 +1,13 @@
+CREATE DATABASE IF NOT EXISTS pdvdb;
+
+USE PDVDB;
 CREATE TABLE IF NOT EXISTS users (
     id int PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255),
     password VARCHAR(255)
 );
 
-CREATE TABLE produtos (
+CREATE TABLE IF NOT EXISTS produtos (
     id int PRIMARY KEY AUTO_INCREMENT,
     codigo VARCHAR(50),
     descricao VARCHAR(255),
@@ -12,10 +15,21 @@ CREATE TABLE produtos (
     preco DOUBLE
 );
 
-CREATE TABLE vendas (
+CREATE TABLE IF NOT EXISTS vendas (
     id int PRIMARY KEY AUTO_INCREMENT,
     data DATE,
     valor DECIMAL(10, 2),
-    pago BOOLEAN
+    pago BOOLEAN,
     forma_pagamento VARCHAR(50)
 );
+
+INSERT INTO `users`(`username`, `password`) VALUES ('lawtrel','123');
+INSERT INTO `users`(`username`, `password`) VALUES ('caiq','321');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('1','CAFE','1','2.50');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('2','MONSTER','1','11');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('3','CACHAÇA','1','20');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('4','REDLABEL','1','119.90');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('5','FANDANGOS','1','5');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('6','LAYS','1','12.90');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('7','MALBORO','1','13.50');
+INSERT INTO `produtos`(`codigo`,`descricao`,`quantidade`,`preco`) VALUES ('8','BLACK','1','19.90');
